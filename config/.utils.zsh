1181fd81e6ef29002e5dffbd3f5e8ef67077b54c
Connor Atherton <c.liam.atherton@gmail.com>
2017-03-17 18:17:59 -0700

    Change to use spinner interface

--- config/.utils.zsh
+++ config/.utils.zsh
@@ -1,5 +1,143 @@
 #!/bin/zsh
 
-function exists() {
+# Checks if a command exists in the current environment.
+#
+# $1 - Command to check for
+exists() {
   command -v "$1" &>/dev/null
+}
+
+# Find Operating system
+os() {
+  local os=
+
+  case uname in
+    Linux*) os=linux ;;
+    Darwin*) os=darwin ;;
+  esac
+
+  echo "$os"
+}
+
+# Remove a file from the home directory.
+#
+# $1 - Name of file to remove
+remove_from_home () {
+  FILEPATH=$HOME/$1
+
+  if [[ -a $FILEPATH ]]; then
+
+    if [[ -d $FILEPATH ]]; then
+      rm -rf $FILEPATH
+    elif [ -h $FILEPATH ]; then
+      unlink $FILEPATH
+    else
+      rm $FILEPATH
+    fi
+  fi
+}
+
+#
+# Text functions
+#
+
+# Transforms a newline string to a comma string
+#
+# $1 - input string with newline chars
+newline_join() {
+  echo -e "$1" | tr -s '\r\n' ', ' | sed -e "s/, $//"
+}
+
+#
+# Output functions
+#
+
+# Prints a message with a specific color. Mainly used for debug messages.
+#
+# $1 - ANSII color code
+# $2 - Message to print
+print_color() {
+  echo -e "\e[38;05;${1}m ${2}"
+}
+
+local prependStr="==>"
+
+print_yellow() { print_color 33 "$prependStr $1" }
+print_green()  { print_color 48 "$prependStr $1" }
+print_blue()   { print_color 32 "$prependStr $1" }
+print_gray()   { print_color 8 "$prependStr $1" }
+print_red()    { print_color 196 "$prependStr $1" }
+debug()        { print_red "[DEBUG] $prependStr $1" }
+
+# Prints the dotfiles header. Only useful for the install script.
+print_dotfiles_header() {
+  print_color 201 "  __  _ _____              ___ _ _"
+  print_color 13  " /  \/ |____ \       _    / __|_) |"
+  print_color 123 "(_/\__/ _   \ \ ___ | |_ | |__ _| | ____  ___"
+  print_color 155 "       | |   | / _ \|  _)|  __) | |/ _  )/___)"
+  print_color 180 "       | |__/ / |_| | |__| |  | | ( (/ /|___ |"
+  print_color 201 "       |_____/ \___/ \___)_|  |_|_|\____|___/"
+  echo ""
+}
+
+up_n_lines() {
+  echo -en "\e[${1}A"
+}
+
+#
+# Spinner functions
+#
+
+# Starts the spinner and places the job in the background so we can do other work.
+#
+# $1 - Optional message to display with the spinner
+start_spinner() {
+  echo ""
+  spinner_loop "$1" &
+
+  # Store these as globals for when we stop
+  spinner_message=$1
+  spinner_pid=$!
+
+  disown
+}
+
+# The actual loop of the spinner.
+#
+# $1 - Optional message to display with the spinner
+#
+# private
+spinner_loop() {
+  local delay=0.1
+  local spinnerChars='\|/-'
+  local length=${#spinnerChars}
+
+  i=0
+
+  # start spinning
+  while true
+  do
+    i=$(($i+1))
+    charIndex=$(($i%$length))
+    char=${spinnerChars:$charIndex:1}
+
+    up_n_lines 1
+
+    print_green "$char $1"
+    sleep $delay
+  done
+}
+
+stop_spinner() {
+  local pid="${spinner_pid}"
+
+  if [[ -z $pid ]]; then
+    print_red "Tried to stop spinner but it was not running"
+    exit 1
+  fi
+
+  kill $pid >/dev/null 2>&1
+
+  up_n_lines 1
+  print_green "✔ $spinner_message"
 }
--- install.sh
+++ install.sh
@@ -1,44 +1,10 @@
 #!/bin/zsh
 
-# set -e
+# set -eu
 
-# for c in {0..255} ; do
-#     echo -e "\e[38;05;${c}m ${c} Bash Prompt Color Chart"
-# done
-# exit 0
-
-#######################
-#
-# Move to helpers file.
-#
-#######################
-
-#
-# Find Operating system
-#
-os() {
-  local os=
-
-  case uname in
-    Linux*) os=linux ;;
-    Darwin*) os=darwin ;;
-  esac
-
-  bcho "$os"
-}
-
-# $1 - ANSII color code
-# $2 - Message to print
-print_color() {
-  echo -e "\e[38;05;${1}m ${2}"
-}
-
-print_yellow() { print_color 33 "$1" }
-print_green()  { print_color 48 "$1" }
-print_blue()   { print_color 32 "$1" }
-print_gray()   { print_color 8 "$1" }
-print_red()    { print_color 196 "$1" }
-debug()        { print_red "[DEBUG] $1" }
+# $0 is buggy as hell but we only ever do this one time. The rest of the scripts looks for
+# the utils files in the home directory after this file runs.
+source $(dirname $0)/config/.utils.zsh
 
 step() {
   print_green "[ ] $1"
@@ -48,48 +14,27 @@ finish() {
   echo -en "\e[1A"; echo -e "\e[0K\r [x] $version. Done."
 }
 
+# start_spinner
+# sleep 2
+# stop_spinner
 
-print_color 201 "  __  _ _____              ___ _ _"
-print_color 13  " /  \/ |____ \       _    / __|_) |"
-print_color 123 "(_/\__/ _   \ \ ___ | |_ | |__ _| | ____  ___"
-print_color 155 "       | |   | / _ \|  _)|  __) | |/ _  )/___)"
-print_color 180 "       | |__/ / |_| | |__| |  | | ( (/ /|___ |"
-print_color 201 "       |_____/ \___/ \___)_|  |_|_|\____|___/"
-echo ""
-
-#
-# Remove a file from the
-# home dir
-#
-remove_from_home () {
-  FILEPATH=$HOME/$1
-
-  if [[ -a $FILEPATH ]]; then
-
-    if [[ -d $FILEPATH ]]; then
-      rm -rf $FILEPATH
-    elif [ -h $FILEPATH ]; then
-      unlink $FILEPATH
-    else
-      rm $FILEPATH
-    fi
-  fi
-}
+# Kick it off, maestro..
+print_dotfiles_header
 
 #
 # Symlink files back into correct dir
 #
-print_blue "==> Replacing all config files..."
+start_spinner "Replacing all config files"
 find ./config ! -path . -maxdepth 1 -iname ".*" | while read file
 do
   local name=$(basename $file)
 
   remove_from_home $name
-  print_gray "==> Symlinking to $HOME/$name"
   ln -fs $PWD${file:1} $HOME/$name;
 done
+stop_spinner
 
-print_blue "==> Replacing all vim files..."
+start_spinner "Replacing all vim files"
 find ./.vim ! -path . -maxdepth 1 -iname ".*" | while read file
 do
   if echo basename "$file" | grep -E '^.git*' >/dev/null; then
@@ -99,20 +44,19 @@ do
   local name=$(basename $file)
 
   remove_from_home $name
-  print_gray "==> Symlinking to $HOME/$name"
   ln -fs $PWD${file:1} $HOME/$name;
 done
-print_green "==> Done."
+stop_spinner
 
-print_blue "==> Checking for oh-my-zsh..."
+start_spinner "Checking for oh-my-zsh..."
 if ! [ -e ~/.oh-my-zsh ]; then
-  print_red "==> Install oh-my-zsh and run it again."
-  print_red "==> "
-  print_red '==> sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
-  print_red "==> "
+  print_red "Install oh-my-zsh and run it again."
+  print_red ""
+  print_red 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+  print_red ""
   exit 1
 fi
-print_green "==> Done."
+stop_spinner
 
 #
 # TODO: include this above in find
@@ -123,17 +67,17 @@ ln -fs $PWD/functions $HOME/functions
 #
 # install vim.plug to manage deps
 #
-print_blue "==> Configuring vim..."
+start_spinner "Configuring vim"
 if ! [ -e ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-print_green "==> Done."
+stop_spinner
 
 if [ os = "Linux" ]; then
-  print_blue "==> Installing files for Linux."
+  start_spinner "Installing files for Linux"
 else
-  print_blue "==> OSX detected: installing relevant files."
+  start_spinner "OSX detected: installing relevant files"
 
   # permissions
   # sudo chown -R $(whoami):admin /usr/local
@@ -147,27 +91,27 @@ else
     xcode-select --install
   fi
 fi
-print_green "==> Done."
+stop_spinner
 
 #
 # config for linux and mac
 #
-print_blue "==> Installing latest Ruby versions"
-rbenv install -l | grep "^\s*\d.\d.\d$" | sort -r | head -n 3 | sed s/' '/''/g | sort | while read -r version; do
-  print_green "==> $version"
+local ruby_versions=$(rbenv install -l | grep "^\s*\d.\d.\d$" | sort -r | head -n 3 | sed s/' '/''/g | sort)
+start_spinner "Installing latest Ruby versions: $(newline_join $ruby_versions)"
+echo $ruby_versions | while read -r version; do
   rbenv install $version &>/dev/null
-  echo -en "\e[1A"; echo -e "\e[0K\r ==> $version. Done."
   rbenv global $version
 done
-print_green "==> Done."
+stop_spinner
 
-print_blue "==> Installing nvm and setting node version"
+start_spinner "Installing nvm and setting node version"
 . $PWD/scripts/nvm.zsh
-print_green "==> Done."
+stop_spinner
 
-print_blue "==> Installing npm modules"
+start_spinner "Installing npm modules"
 . $PWD/scripts/npm.zsh
-print_green "==> Done."
+stop_spinner
 
+start_spinner "Sourcing rc file"
 . ~/.zshrc
-
+stop_spinner
