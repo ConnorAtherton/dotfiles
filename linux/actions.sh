ce17d5d466e3518ddedf54f253880d96b03f7407
Connor Atherton <connor@connoratherton.com>
2018-11-29 22:06:49 -0800

    add docker script install action

--- linux/actions.sh
+++ linux/actions.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
 update-alternatives --set x-terminal-emulator /usr/bin/urxvt
 
 # Install Docker
+# Add Docker’s official GPG key:
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Check the GPG fingerprint successfully added (should see output from this command)
+sudo apt-key fingerprint 0EBFCD88
+sudo apt-get install docker-ce
+
+# Add the Docker repository for the Xenial build
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   xenial \
+   stable"
+
+# Create a new docker group
+sudo groupadd docker
+
+# Add your user to the docker group.
+# This script assumes that your current user
+# is the one you want to be a docker admin
+sudo usermod -aG docker $USER
