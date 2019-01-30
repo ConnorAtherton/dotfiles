3538dd3f954c39e1bfa2ffa33a58e699498f1772
Connor Atherton <connor@connoratherton.com>
2019-01-30 13:02:31 -0800

    Add Ethan to list of people who can access the git server

--- infra/stacks/git.sh
+++ infra/stacks/git.sh
@@ -3,15 +3,15 @@
 docker run --restart always -d \
   --name git_ui \
   -p 127.0.0.1:8080:8080 \
-  -e BASE_GIT_URL="git@gits" \
-  -e SITE_TITLE="Code collection" \
+  -e BASE_GIT_URL="git@playground" \
+  -e SITE_TITLE="Code Playground" \
   -v "/mnt/persistent_git_storage:/home/git:ro" \
   -w /home/git \
   r.j3ss.co/gitiles
 
 docker run --restart always -d \
   --name git_server \
-  -p 127.0.0.1:2222:22 \
-  -e "PUBKEY=$(cat ~/.ssh/authorized_keys)" \
+  -p 0.0.0.0:2222:22 \
+  -e "PUBKEY=$(cat /var/stacks/authorized_git_keys)" \
   -v "/mnt/persistent_git_storage:/home/git" \
   r.j3ss.co/gitserver
